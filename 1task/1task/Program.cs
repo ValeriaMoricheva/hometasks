94ca3a4c7f8d8552f3b52387b9e774d06d767a06
Valeria.Moricheva@urfu.me <Valeria.Moricheva@urfu.me>
2021-10-28 17:47:51 +0500

    Выполнено задание 1

---
+++ 1task/1task/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Всё тихо — полная луна…");
+
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Всё тихо — полная луна");
+            Console.ReadKey();
+            Console.WriteLine("Блестит меж ветел над прудом,");
+            Console.ReadKey();
+            Console.WriteLine("И возле берега волна");
+            Console.ReadKey();
+            Console.WriteLine("С холодным резвится лучом.");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Всё тихо — полная луна…");
+            Console.WriteLine("Всё тихо — полная луна");
+            Console.WriteLine("Блестит меж ветел над прудом,");
+            Console.WriteLine("И возле берега волна");
+            Console.WriteLine("С холодным резвится лучом.");
+
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
