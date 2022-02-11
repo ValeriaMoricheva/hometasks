ff157e9b5ed61bd5daf47a2a935128189001a54d
Valeria.Moricheva@urfu.me <Valeria.Moricheva@urfu.me>
2022-02-11 16:08:28 +0500

    Выполнено задание 2

---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3;
+            double h = 5;
+            double res = a * a;
+            double ress = res * Math.Sqrt(3.0);
+            double square = ress / 4.0;
+
+            Console.WriteLine("Если длина стороны правильного треугольника, лежащего в основании пирамиды равна " + a + ", а высота пирамиды равна " + h);
+            Console.ReadLine();
+            Console.WriteLine("то:");
+            Console.ReadLine();
+
+            Console.WriteLine("Площадь основания пирамиды равна:" + square);
+            Console.ReadLine();
+
+            double divide = square / 3;
+
+            Console.WriteLine("Объем пирамиды равен:" + divide * h);
+            Console.ReadLine();
+        }
+    }
+}
